4fc31375dc88a30d71ec5d00c78f86c038622f16
Andrey Gridyaev <agridyaev@gmail.com>
2021-03-28 16:29:53 +0300

    sys_lib: change sys.argv example

--- sys_lib/02_argv.py
+++ sys_lib/02_argv.py
@@ -1,16 +1,5 @@
-import argparse
 import sys
-import time
+print(f'list: {sys.argv}')
 
-
-def sleep_sec(x):
-    print(sys.argv)
-    time.sleep(x)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', type=int, default=60)
-    args = parser.parse_args()
-
-    sleep_sec(args.s)
+for i in range(len(sys.argv)):
+    print(f'{i}, arg: {sys.argv[i]} {type(sys.argv[i])}')
