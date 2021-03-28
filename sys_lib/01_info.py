dde81c2cd40a01468ed3576ed0f8146d49789ba7
Andrey Gridyaev <agridyaev@gmail.com>
2021-03-28 16:29:12 +0300

    sys_lib/01_info: remove redundant code

--- sys_lib/01_info.py
+++ sys_lib/01_info.py
@@ -1,11 +1,5 @@
 import sys
 
-
-def print_info():
-    print(f'Python version: {sys.version}')
-    print(f'Platform: {sys.platform}')
-    print(f'Executable path: {sys.executable}')
-
-
-if __name__ == '__main__':
-    print_info()
+print(f'Python version: {sys.version}')
+print(f'Platform: {sys.platform}')
+print(f'Executable path: {sys.executable}')
