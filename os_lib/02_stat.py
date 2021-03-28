227e622fb37919865d2d7a6045ea3c6c4eab9b9d
Andrey Gridyaev <agridyaev@gmail.com>
2021-03-28 20:29:18 +0300

    os_lib: add os.stat example

---
+++ os_lib/02_stat.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import time
+
+if len(sys.argv) == 1:
+    filename = __file__
+else:
+    filename = sys.argv[1]
+
+stat_info = os.stat(filename)
+
+print(f'os.stat({filename}):')
+print(f'  Size: {stat_info.st_size}')  # in bytes
+print(f'  Permissions: {oct(stat_info.st_mode)}')  # man 2 stat
+print(f'  Owner: {stat_info.st_uid}')  # dscl . -read /Users/<username> | grep UniqueID
+print(f'  Created      : {time.ctime(stat_info.st_ctime)}')
+print(f'  Last modified: {time.ctime(stat_info.st_mtime)}')
+print(f'  Last accessed: {time.ctime(stat_info.st_atime)}')
