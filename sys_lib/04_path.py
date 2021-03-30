cc9de17fad73793cf9a6795a67e8bc8a721e2bc1
Andrey Gridyaev <agridyaev@gmail.com>
2021-03-30 15:33:55 +0300

    sys_lib/04_path: change order for printing orig sys.path

--- sys_lib/04_path.py
+++ sys_lib/04_path.py
@@ -1,11 +1,11 @@
 import sys
 import pprint
 
+pprint.pprint(f'Default paths: {sys.path}')
 try:
     from module_1 import name
 except ModuleNotFoundError as e:
     print(e)
-pprint.pprint(f'Default paths: {sys.path}')
 sys.path.append('/Users/agridyaev/OTUS/python_linux/pack')
 pprint.pprint(f'Updated paths: {sys.path}')
 from module_1 import name
